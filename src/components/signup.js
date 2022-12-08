032ab45a898ed4455958813d4018eb277931238e
Jaffar Hussein <jaffhussein@icloud.com>
2022-12-08 12:39:29 +0100

    ft:Adds the sign up form validation

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,7 +1,11 @@
 import '../style.css';
 import signup from '../assets/login.png'
+import { useForm } from 'react-hook-form';
 
 function Signup(){
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors.phonenumber?.message);
 return(
 <>
 <div className="row g-0">
@@ -17,23 +21,50 @@ return(
          <div className="row ">
            <div className="col-md-9 col-lg-8   mx-auto ">
              <h3 className="login-heading mb-4 text-white text-center" >Create Account</h3>
-             <form  className="box" role="form">
+             <form onSubmit={handleSubmit(onSubmit)}  role="form" className="box needs-validation"  noValidate>
               <div className='form-floating mb-3'>
-              <input type="text"  name="username" className="form-control userName text-white" id="floatingInput" placeholder="Enter your nickname"/>
+              <input type="text"  name="username"  className={`form-control userName text-white ${errors.username ? 'is-invalid' : ''}`}  id="floatingInputUsername" placeholder="firstname"
+              {...register("username", {required: "Please input your username", minLength: {value:5, message:"Username is too short"}})} 
+              />
                  <label className='text-secondary' >Enter your username</label>
+                 <div className="invalid-feedback" id="floatingInputUsernameFeedback">{errors.username?.message}</div>
               </div>
+              <div className="form-floating mb-3 has-validation">
+                  <input type="tel" name="username" className={`form-control userName text-white ${errors.phonenumber ? 'is-invalid' : ''}`} id="floatingInput" placeholder="phonenumber"
+                   {...register(
+                    "phonenumber", 
+                    {required : "Input your phone number to login"
+                   , min: 
+                   {
+                    value:12,
+                    message:"The phone number is too short"
+                  }, pattern:{ 
+                    value:/^\+254/i,
+                    message:"Input your phone number +254.."
+                  }})}  
+
+                   />
+                  <label className='text-white' >+254</label>
+                  <div className="invalid-feedback" id="floatingInputFeedback">{errors.phonenumber?.message}</div>
+                </div>
                <div className="form-floating mb-3">
-                 <input type="phone"  name="username" className="form-control userName text-white" id="floatingInput" placeholder="name@example.com"/>
-                 <label className='text-secondary' >+254</label>
-               </div>
-               <div className="form-floating mb-3">
-                 <input type="email"  name="email" className="form-control userName text-white" id="floatingInput" placeholder="name@example.com"/>
+                 <input type="email"  name="email" className={`form-control userName text-white ${errors.email ? 'is-invalid' : ''}`}  id="floatingEmail" placeholder="email"
+                  {...register("email", {required: "Please input your Email",
+                  pattern:{
+                    value:/^\S+@\S+$/i,
+                    message:"Input a valid email"
+                  }
+                })} 
+                 />
                  <label className='text-secondary' >Enter email address</label>
+                 <div className="invalid-feedback" id="floatingEmailFeedback">{errors.email?.message}</div>
                </div>
                <div className="form-floating mb-3">
-                 <input type="password"  name="password" className="form-control pass" id="floatingPassword" placeholder="Password"/>
-                 <label className='text-secondary' >Enter Password</label>
-               </div>
+                  <input type="password" name="password" className={`form-control pass ${errors.password ? 'is-invalid' : ''} `}id="floatingPassword" autoComplete="current-password webauthn" placeholder="password"
+                   {...register("password", {required: "Please input your password"})} />
+                  <label className='text-white' >Enter Password</label>
+                  <div className="invalid-feedback" id="floatingPasswordFeedback">{errors.password?.message}</div>
+                </div>
 
            
 
