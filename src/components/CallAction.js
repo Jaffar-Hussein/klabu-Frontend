0d641213425d9d23d8698edb71a218860e1f9916
Jaffar Hussein <jaffhussein@icloud.com>
2022-12-08 10:16:51 +0300

    ft:callAction-finished call action

--- src/components/CallAction.js
+++ src/components/CallAction.js
@@ -2,28 +2,45 @@ import foodA from '../assets/foodA.png'
 import foodB from '../assets/FoodB.png'
 import '../css/main.css'
 import { Link } from "react-router-dom"
-
+import Blob from '../assets/blob.svg'
+import RecipeBook from '../assets/RecipeBook.svg'
 
 function CallAction() {
     return (
-        <div className="container my-5">
-            <div className="row">
-                <div className="col-md-6 col-lg-6">
+        <>
+            <div className="d-flex align-items-center call">
+                {/* <div className="col-md-6 col-lg-6 "> */}
+                <div>
+                    <img className="img-fluid" src={Blob} />
 
                 </div>
-                <div className="col-md-6 col-lg-6">
+                {/* </div> */}
+                {/* <div className="col-md-6 col-lg-6 "> */}
+                <div>
                     <p className='h4'>Enjoy lots of free receipes</p>
-                    <p>As soon as the temperature starts to drop, we
-                        break out this collection of favorite soups, stews
-                        casseroles, and meaty classics.</p>
-                        <Link>
-                        <button className="btn btn-primary rounded-pill ">
+                    <div>
+                        <p>As soon as the temperature starts to drop,</p>
+
+                    </div>
+                    <div>
+                        <p> we break out this collection of favorite soups, stews
+                            casseroles, and meaty classics.</p>
+                    </div>
+                    <Link>
+                        <button className="btn btn-primary rounded-pill my-3">
                             Upload Recipe
                         </button>
                     </Link>
                 </div>
+                <div className='recipeBook'>
+                    <img src={RecipeBook} className="img-fluid" />
+
+                </div>
+                {/* </div> */}
             </div>
-        </div>
+
+        </>
+
     )
 }
-export default CallAction
+export default CallAction;
